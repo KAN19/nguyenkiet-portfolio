e6cec67f0d959a68b6674033f18c5e3b14e2021b
Kiet Nguyen <6393445-19520666NK@users.noreply.gitlab.com>
2022-06-25 23:05:29 +0700

    finish template

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FiTwitter } from "react-icons/fi";
+import "./footer.css";
 
 function Footer() {
-	return <div>Footer</div>;
+	return (
+		<footer>
+			<a href="#" className="footer__logo">
+				NGUYEN KIET
+			</a>
+
+			<ul className="permalinks">
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#about">About</a>
+				</li>
+				<li>
+					<a href="#experience">Experience</a>
+				</li>
+				<li>
+					<a href="#portfolio">Portfolio</a>
+				</li>
+				<li>
+					<a href="#testimonials">Testimonials</a>
+				</li>
+				<li>
+					<a href="#contact">Contact</a>
+				</li>
+			</ul>
+
+			<div className="footer__socials">
+				<a href="https://facebook.com">
+					<FaFacebookF />
+				</a>
+				<a href="https://instagram.com">
+					<FaInstagram />
+				</a>
+				<a href="https://twitter.com">
+					<FiTwitter />
+				</a>
+			</div>
+
+			<div className="footer__copyright">
+				<small>&copy; NguyenKiet. All rights reserved</small>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;
--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,7 +1,68 @@
 import React from "react";
+import AVTR1 from "assets/avatar1.jpg";
+import AVTR2 from "assets/avatar2.jpg";
+import AVTR3 from "assets/avatar3.jpg";
+import AVTR4 from "assets/avatar4.jpg";
+import "./testimonial.css";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+	{
+		avatar: AVTR1,
+		name: "Tina Snow",
+		review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eos deserunt, nemo hic quaerat dolore.",
+	},
+	{
+		avatar: AVTR2,
+		name: "Shatta Wale",
+		review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eos deserunt, nemo hic quaerat dolore.  ",
+	},
+	{
+		avatar: AVTR3,
+		name: "Tina Snow",
+		review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eos deserunt, nemo hic quaerat dolore. ",
+	},
+	{
+		avatar: AVTR4,
+		name: "Tina Snow",
+		review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eos deserunt, nemo hic quaerat dolore.",
+	},
+];
 
 function Testimonial() {
-	return <section id="testimonial">Testimonial</section>;
+	return (
+		<section id="testimonial">
+			<h5>Review from clients</h5>
+			<h2>Testimonials</h2>
+
+			<Swiper
+				className="container testimonials__container"
+				modules={[Pagination]}
+				spaceBetween={40}
+				slidesPerView={1}
+				pagination={{ clickable: true }}
+			>
+				{data &&
+					data.map(({ avatar, name, review }, index) => (
+						<SwiperSlide key={index} className="testimonial">
+							<div className="client__avatar">
+								<img src={avatar} alt="" />
+							</div>
+							<h5 className="client__name">{name}</h5>
+							<small className="client__review">{review}</small>
+						</SwiperSlide>
+					))}
+			</Swiper>
+		</section>
+	);
 }
 
 export default Testimonial;
